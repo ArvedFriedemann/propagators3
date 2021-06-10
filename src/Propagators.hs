dd2d11b1ac2a06dca69bddcf258766e7378e015a
arved <arved.friedemann@stoppe.de>
2021-06-10 17:11:10 +0200

    Made the predicate to express that something might not be instantiated

--- src/Propagators.hs
+++ src/Propagators.hs
@@ -51,65 +51,41 @@ write adr val = MV.mutate adr update >>= mapM_ (forkExec . crcont)
           then (getProps v,[])
           else notify mt (getProps v)
 
-addPropagator :: (MonadMutate m v, HasValue k a, HasProps m v k a ) =>
-  v k -> (a -> Bool) -> (a -> m ()) -> m ()
+addPropagator :: (MonadMutate m v, HasValue k a, HasProps m v k a) =>
+  v k -> (a -> Instantiated) -> (a -> m ()) -> m ()
 addPropagator p pred cont =
-  join $ MV.mutate p $ \v ->
-      if pred (getVal v)
-      then (v, cont $ getVal v)
-      else (setProps v (ContRec p pred (read p >>= cont) : getProps v), return ())
+  join $ MV.mutate p $ \v -> case pred (getVal v) of
+      Failed -> (v, return ())
+      Instance -> (v, cont $ getVal v)
+      NoInstance -> (setProps v (ContRec p pred (read p >>= cont) : getProps v), return ())
 
 --second collection is the succeeding propagators, first is the failed one that needs to be written back
 notify :: a -> PCollection m v k a -> (PCollection m v k a, PCollection m v k a)
-notify val props = partition (not . ($ val) . crpred) props
+notify val props = (inst, noInst)
+  where (_, noInst, inst) = splitInstantiated props (($ val) . crpred)
 
 
 data ContRec m v k a = ContRec {
   crptr :: v k, --might not be needed
-  crpred :: (a -> Bool),
+  crpred :: a -> Instantiated,
   crcont :: m ()
 }
+
+
 
 type PCollection m v k a = [ContRec m v k a]
 
 --TODO: make monad instance from those
-data Instantiated a = Failed | NoInstance | Instance a
-  deriving (Show, Eq, Ord, Functor)
+data Instantiated = Failed | NoInstance | Instance
+  deriving (Show, Eq, Ord)
 
-instance Applicative Instantiated where
-  pure = Instance
-  (Instance f) <*> (Instance x) = Instance (f x)
-  Failed <*> _ = Failed
-  NoInstance <*> _ = NoInstance
-  (Instance _) <*> Failed = Failed
-  (Instance _) <*> NoInstance = NoInstance
-
-instance Monad Instantiated where
-  (Instance v) >>= fkt = fkt v
-  Failed >>= _ = Failed
-  NoInstance >>= _ = NoInstance
-
--- failIfNotApplicable :: (s :<: t) => (s -> Bool) -> (t -> MaybeFails)
--- failIfNotApplicable (something in s) = run the pred, if fails then Fail
--- failIfNotApplicable (something not in s) = Impossible
--- failIfNotApplicable (not instantiated enough) = NotInstantiatedEnough
+splitInstantiated :: [a] -> (a -> Instantiated) -> ([a],[a],[a])
+splitInstantiated lst f = (filter ((== Failed) . f) lst
+                          ,filter ((== NoInstance) . f) lst
+                          ,filter ((== Instance) . f) lst)
 
 --TODO: when splittin for constructors, we need pref as a -> Maybe b
 --This needs to go into a tree way...Conflict, Unassigned and Just ... to completely cover constructors
 iff :: (MonadMutate m v, HasValue k a, HasProps m v k a) =>
-  v k -> (a -> Bool) -> (a -> m ()) -> m ()
+  v k -> (a -> Instantiated) -> (a -> m ()) -> m ()
 iff p pred m = addPropagator p pred m
-
-{-
-empty :: List a -> Instantiated ()
-cons :: List a -> Instantiated (a, List a)
-
-Blah Double String
-blah :: Blah a b -> Instantiated (a,b)
-
-
-blah = do
-  iff x prop1 $ do
-    iff y prop2 $ do
-      -- do something with them
--}
