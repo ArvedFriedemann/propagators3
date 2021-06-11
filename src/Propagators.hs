d98020987b128078dee743683a0bb9861b2f9482
arved <arved.friedemann@stoppe.de>
2021-06-11 12:13:30 +0200

    Revert "[WIP] Made first structure for dereferencing pointers"
    This reverts commit 02094c3873fceec07717d61ebb1a774070651676.

--- src/Propagators.hs
+++ src/Propagators.hs
@@ -34,26 +34,17 @@ class (HasValIn k (PCollection m v k a)) => HasProps m v k a | k -> a where
 class HasEmpty k where
   empty :: k
 
-class StackedPointer m kp k | kp -> k, kp -> m where
-  deRef :: kp -> m k
-  mutate :: kp -> (k -> (k,a)) -> m a
-  mutate_ :: kp -> (k -> k) -> m ()
-  mutate_ k f = mutate k (\x -> (f x, ()))
-  createSP :: k -> m kp
-
-new :: forall m v kp k a.
-  (MonadNew m v, HasEmpty k, HasValue k a, HasProps m v k a,
-  StackedPointer m (v kp) k) => a -> m (v kp)
-new val = createSP $ setProps @m @v [] . setVal val $ empty
+new :: forall m v k a. (MonadNew m v, HasEmpty k, HasValue k a, HasProps m v k a) => a -> m (v k)
+new val = MV.new $ setProps @m @v [] . setVal val $ empty
 
 --read p >>= m ~> watch p (read p >>= m)
-read :: (MonadRead m v, HasValue k a, StackedPointer m (v kp) k) => v kp -> m a
-read adr = getVal <$> (deRef adr)
+read :: (MonadRead m v, HasValue k a) => v k -> m a
+read adr = (getVal <$> (MV.read adr))
 
-write :: forall m v kp k a.
-  (MonadFork m, HasValue k a, HasProps m v k a, StackedPointer m (v kp) k, Eq a, Lattice a) =>
-  v kp -> a -> m ()
-write adr val = mutate adr update >>= mapM_ (forkExec . crcont)
+write :: forall m v k a.
+  (MonadFork m, MonadMutate m v, HasValue k a, HasProps m v k a, Eq a, Lattice a) =>
+  v k -> a -> m ()
+write adr val = MV.mutate adr update >>= mapM_ (forkExec . crcont)
   where
     update :: k -> (k,PCollection m v k a)
     update v = (setProps nosuccprops (setVal mt v), succprops)
@@ -65,10 +56,10 @@ write adr val = mutate adr update >>= mapM_ (forkExec . crcont)
           then (getProps v,[])
           else notify mt (getProps v)
 
-addPropagator :: (HasValue k a, HasProps m v k a, StackedPointer m (v kp) k) =>
-  v kp -> (a -> Instantiated) -> (a -> m ()) -> m ()
+addPropagator :: (MonadMutate m v, HasValue k a, HasProps m v k a) =>
+  v k -> (a -> Instantiated) -> (a -> m ()) -> m ()
 addPropagator p pred cont =
-  join $ mutate p $ \v -> case pred (getVal v) of
+  join $ MV.mutate p $ \v -> case pred (getVal v) of
       Failed -> (v, return ())
       Instance -> (v, cont $ getVal v)
       NoInstance -> (setProps (ContRec p pred (read p >>= cont) : getProps v) v, return ())
@@ -97,6 +88,6 @@ splitInstantiated lst f = (filter ((== Failed) . f) lst
                           ,filter ((== NoInstance) . f) lst
                           ,filter ((== Instance) . f) lst)
 
-iff :: (HasValue k a, HasProps m v k a, StackedPointer m (v kp) k) =>
-  v kp -> (a -> Instantiated) -> (a -> m ()) -> m ()
+iff :: (MonadMutate m v, HasValue k a, HasProps m v k a) =>
+  v k -> (a -> Instantiated) -> (a -> m ()) -> m ()
 iff p pred m = addPropagator p pred m
