ca22e032cec74162d6a69adcab8febe5a2921102
arved <arved.friedemann@stoppe.de>
2021-08-05 14:10:03 +0200

    Found out that propagators are not executed correctly when notifying between scopes

--- src/RunPropagatorInterface.hs
+++ src/RunPropagatorInterface.hs
@@ -9,6 +9,7 @@ import "mtl" Control.Monad.Trans
 import "mtl" Control.Monad.Reader
 import "base" Debug.Trace
 import "base" Control.Monad.IO.Class
+import "base" Control.Concurrent
 import "containers" Data.Set (Set)
 import qualified "containers" Data.Set as S
 
@@ -23,34 +24,46 @@ testDisj = runPropM @v $ do
     ]
   readUpdate v1 (lift . putStrLn . (++" in orig") . show)
 
-testDisj' :: forall m v. (MonadIO m, MonadProp m v) => m ()
+testDisj' :: forall m v. (MonadIO m, MonadProp m v, PropUtil m) => m ()
 testDisj' = do
   v1 <- new' @_ @v []
+  getScope >>= \s -> liftIO $ putStrLn $ "currScope: "++show s
   outruled <- new @_ @v
   --write outruled $ RS $ S.singleton 2 --this just causes the outruled list to have one element. Everything should be promoted now...
   scoped @_ @v $ do
     let i = 0
-    write v1 ["b"]
-    readUpdate outruled (liftIO . putStrLn . (++" outruled in scope0") . show)
-    readUpdate v1 (liftIO . putStrLn . (++" in scope0") . show)
+    write v1 ["a"]
+    getScope >>= \s -> liftIO $ putStrLn $ "scope1: "++show s
+    --write outruled $ RS $ S.singleton 2
+    --readUpdate outruled (liftIO . putStrLn . (++" outruled in scope0") . show)
+    --readUpdate v1 (liftIO . putStrLn . (++" in scope0") . show)
     iffb v1 (\c -> c /= ["a"] && c /= []) $ parScoped @_ @v $ write outruled (RS $ S.singleton i)
-    iffb outruled (\(RS s) -> (S.size s == 1) && (not $ S.member i s)) $
-      (liftIO . putStrLn  $ "chose "++show i) >> promote v1
+    --iffb outruled (\(RS s) -> trace ("current outruled : " ++ show s++" matches "++show (S.size s == 1)) (S.size s == 1)) $
+      --(liftIO $ threadDelay 1000) >> (liftIO . putStrLn  $ "chose "++show i) >> promote v1
+    iff outruled (\(RS s) -> if S.null s then trace "NoInstanceCase" NoInstance else trace "Instance Case" Instance ()) $
+      const $ (getScope >>= \s -> liftIO $ putStrLn $ "currScopeExec: "++show s) >> promote v1
+      --TODO: scope of execution wrong!
+    --promote v1
+    --(liftIO $ threadDelay 100)
+    --(parScoped @_ @v $ readState outruled) >>= (\v -> write outruled v)
     return ()
   scoped @_ @v $ do
     let i = 1
-    write v1 ["a"]
-    readUpdate outruled (liftIO . putStrLn . (++" outruled in scope1") . show)
-    readUpdate v1 (liftIO . putStrLn . (++" in scope1") . show)
+    write v1 ["b"]
+    --readUpdate outruled (liftIO . putStrLn . (++" outruled in scope1") . show)
+    --readUpdate v1 (liftIO . putStrLn . (++" in scope1") . show)
     iffb v1 (\c -> c /= ["a"] && c /= []) $ parScoped @_ @v $ write outruled (RS $ S.singleton i)
-    iffb outruled (\(RS s) -> (S.size s == 1) && (not $ S.member i s)) $
-      (liftIO . putStrLn  $ "chose "++show i) >> promote v1
+    --iffb outruled (\(RS s) -> (S.size s == 1) && (not $ S.member i s)) $
+      --(liftIO . putStrLn  $ "chose "++show i) >> promote v1
     return ()
   --TODO: for some weird reason, the second promote overrides the first one!
   --TODO: outrule value is not propagated up!
   --TODO: outrule value seems to be overridden...
   readUpdate outruled (liftIO . putStrLn . (++" outruled in orig") . show)
   readUpdate v1 (liftIO . putStrLn . (++" in orig") . show)
+  (liftIO $ threadDelay 2000)
+  v1val <- readState v1
+  liftIO $ putStrLn $ "delayed v1 in orig: "++show v1val
 
 testRun :: forall v. (v ~ UP) => IO ()
 testRun = runPropM @v (testDisj' @_ @(CustPtr (ReaderT (PropState IO v) IO) v))
